f32f4d7785217200549dd800cc3eac636dce0c74
sripiranavan <sripiranavan08@gmail.com>
2020-05-07 00:31:18 +0530

    upto react children property

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi! there i am react app</h1>
+        <h1> Hi!there i am react app </h1>
+        <Person name="Sri" age="28" />
+        <Person name="Piranavan" age="27">
+          My Hobbies : Learn New Programming
+        </Person>
+        <Person name="Indirani" age="65" />
       </div>
     );
     // return React.createElement(
