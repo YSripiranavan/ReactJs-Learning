3359f2ba5a15f211ee7380058410bc9d07fd11c8
sripiranavan <sripiranavan08@gmail.com>
2020-05-07 16:42:49 +0530

    Before hooks

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Sri", age: 28 },
+      { name: "Piranavan", age: 27 },
+      { name: "Indirani", age: 65 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "SriPiranavan", age: 28 },
+        { name: "Piranavan", age: 27 },
+        { name: "Indirani", age: 65 }
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1> Hi!there i am react app </h1>
-        <Person name="Sri" age="28" />
-        <Person name="Piranavan" age="27">
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
           My Hobbies : Learn New Programming
         </Person>
-        <Person name="Indirani" age="65" />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement(
